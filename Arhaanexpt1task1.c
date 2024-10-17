db57367c56a12d7a80c73adcf704bbd4dffe0b11
Arhaanm11 <arhaan.mulla1103@gmail.com>
2024-10-17 19:03:16 +0530

    Create Arhaanexpt1task1.c

---
+++ Arhaanexpt1task1.c
@@ -0,0 +1,29 @@
+/*Name: Arhaan A Mulla
+UIN:241P049
+Title:WAP to print your name,age,class,division and UIN on the screen. Use escape sequences to properly format the output. Experiment 1, task 1
+Roll Number:46 */
+#include<stdio.h>
+
+
+int main()
+{
+  printf("Name\t\t:\t Arhaan Mulla\n");
+  printf("Age\t\t\t:\t19\n");
+  printf("Class\t:\tFirst Year\n");
+  printf("Div\t\t\t:\tC3\n");
+  printf("UIN\t\t\t:\t241p049\n");
+
+
+  return 0;
+
+}
+
+/*Output:
+Name            :       Arhaan Mulla
+Age                     :       19
+Class   :       First Year
+Div                     :       C3
+UIN                     :       241p049
+
+Process returned 0 (0x0)   execution time : 0.072 s
+Press any key to continue.*/
